23da963059ecbce6bb152bc115774b86fd8d78a8
Luís Matos <luismatos@greywolf-entertainment.com>
2017-10-03 12:47:11 +0100

    move f

--- Assets/NewPlayMode.cs
+++ Assets/NewPlayMode.cs
@@ -1,16 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Configuration;
+using System.Security.Policy;
 
 public class NewPlayMode : MonoBehaviour {
+	public float speed = 5;
 
 	// Use this for initialization
 	void Start () {
-		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		this.gameObject.transform.Translate (Vector3.forward * speed * Time.deltaTime);
 	}
 }
